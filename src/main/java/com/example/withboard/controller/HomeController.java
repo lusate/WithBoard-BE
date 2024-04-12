cab35eabf9928758419b356adcf0ccddd2e8f053
“Winsomejoo” <tmdwn0603@naver.com>
2024-04-12 19:31:45 +0900

    Feat:Backend 3 Layer 및 디렉토리 구조 설정

--- src/main/java/com/example/withboard/controller/HomeController.java
+++ src/main/java/com/example/withboard/controller/HomeController.java
@@ -1,7 +1,13 @@
 package com.example.withboard.controller;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("/api/home")
+@RestController
+@RequestMapping("/api/home")
+@RequiredArgsConstructor
+@Slf4j
 public class HomeController {
 }
---
+++ src/main/java/com/example/withboard/repository/HomeRepository.java
@@ -0,0 +1,9 @@
+package com.example.withboard.repository;
+
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface HomeRepository {
+
+}
