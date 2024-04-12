22eccc35e9d5591c8b1d14d6aff6e7ac495d712e
rayrayr <rayrayr@example.com>
2024-04-12 16:28:11 +0900

    Feat: Initializer withboard project

---
+++ src/main/java/com/example/withboard/WithboardApplication.java
@@ -0,0 +1,13 @@
+package com.example.withboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WithboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WithboardApplication.class, args);
+	}
+
+}
